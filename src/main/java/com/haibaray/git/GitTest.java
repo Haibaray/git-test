f880697240749a7950bbfc964ed8345feb696d11
haibara <haibaray@qq.com>
2021-04-28 12:19:48 +0800

    merge commit

--- src/main/java/com/haibaray/git/GitTest.java
+++ src/main/java/com/haibaray/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Test");
         System.out.println("Git Test2");
+        System.out.println("hotfix Test");
         System.out.println("Git Test3");
     }
 }
